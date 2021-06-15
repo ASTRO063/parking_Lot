10782cb8ad31cc4d5bf5f22881454b1a092db18e
ASTRO063 <manikanta.kodimala3@gmail.com>
2021-06-15 16:32:46 +0530

    changes in configuration of  jest to run TypeScript

---
+++ src/car.ts
@@ -0,0 +1,11 @@
+class Car {
+    registration_Number: string;
+    colour: string;
+    constructor(registrationNumber: string, colour: string)
+    {
+        this.registration_Number = registrationNumber;
+        this.colour = colour;   
+    }
+}
+
+export default Car;
